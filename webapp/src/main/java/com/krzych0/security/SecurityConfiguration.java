6ac2c6ad307089ea6d5c8448c603b542364914b2
Krzysztof Kocon <krzysztof.kocon@imgtec.com>
2017-04-07 14:18:32 +0200

    Remove imports.

--- webapp/src/main/java/com/krzych0/security/SecurityConfiguration.java
+++ webapp/src/main/java/com/krzych0/security/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.krzych0.security;
 
-import com.krzych0.utils.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
